0e4b6ec45b38b3a26e3b804d9c2d525ce180b323
齋藤新平 <shinpei31103541@yahoo.co.jp>
2022-01-29 17:09:47 +0000

    アイコン等の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
     validates :password, presence: { if: :current_password }
 
     validates :user_id, presence: { message: "を入力してください" }
-    validates :name, presence: { message: "を入力してください" }
+    validates :name, presence: { message: "を入力してください" },
+            format: { with: /\A[ぁ-んァ-ン一-龥a-zA-Z]+\z/, allow_blank: true, message: "は空白なし、ひらがな,カタカナ,漢字,英文字のみで入力してください。" }
 end
