33e4757346039e9b757b949990914ac6dd107ebd
齋藤新平 <shinpei31103541@yahoo.co.jp>
2022-01-29 14:46:34 +0000

    Heroku用の設定

---
+++ db/seeds/production/users.rb
@@ -0,0 +1,13 @@
+names = %w(Taro Jiro Hana John Mike Sophy Bill Alex Mary Tom)
+fnames = ["佐藤", "鈴木", "高橋", "田中"]
+gnames = ["太郎", "次郎", "花子"]
+
+
+0.upto(9) do |idx|
+    User.create(
+        user_id: names[idx],
+        name: "#{fnames[idx % 4]}#{gnames[idx % 3]}",
+        password: "stms",
+        password_confirmation: "stms"
+    )
+end
