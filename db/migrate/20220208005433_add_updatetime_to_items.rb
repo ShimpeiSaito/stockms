18d35d948ae2d248ea8084c362970dbfad1ae5b4
齋藤新平 <shinpei31103541@yahoo.co.jp>
2022-02-08 02:34:52 +0000

    修正

--- db/migrate/20220208005433_add_updatetime_to_items.rb
+++ db/migrate/20220208005433_add_updatetime_to_items.rb
@@ -1,5 +1,5 @@
 class AddUpdatetimeToItems < ActiveRecord::Migration[5.2]
   def change
-    add_column :items, :update_time, :datetime
+    add_column :items, :update_time, :datetime, default: Time.now
   end
 end
